256cf73e8d0a6d2228a35c5882c75165e187b2b9
Anil Kulkarni <anil@textio.com>
2016-09-28 12:38:35 -0700

    add .* banhammering

--- tests/lib/rules/no-restricted-functions.js
+++ tests/lib/rules/no-restricted-functions.js
@@ -25,6 +25,14 @@ ruleTester.run("no-restricted-functions", rule, {
             code: "foo.thisIsOkay()",
             options: ["thisIsOkay"],
         },
+        {
+            code: "foo();",
+            options: ["foo.*"],
+        },
+        {
+            code: "var foo;",
+            options: ["foo.*"],
+        },
     ],
 
     invalid: [
@@ -42,6 +50,14 @@ ruleTester.run("no-restricted-functions", rule, {
             errors: [{
                 message: "dontCallMe is not allowed",
                 type: "Identifier",
+            }],
+        },
+        {
+            code: "localStorage.setItem('foo', 'bar');",
+            options: ["localStorage.*"],
+            errors: [{
+                message: "localStorage.* is not allowed",
+                type: "MemberExpression",
             }],
         },
     ]
